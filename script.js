ff1da35d7921a122b72433f7621555497df293f8
Hilliard Stamper <hilliard.stamper@att.net>
2021-10-26 12:53:54 -0400

    added a while loop

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var generateBtn = document.querySelector("#generate");
 
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "10"]; 
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "_", "{", "}", "|", "~", "]"];
 
 var confirmLength = "";
 var confirmSpecialCharacter;
@@ -15,16 +17,25 @@ var confirmUppercase;
 var confirmLowercase;
 
 function generatePassword() {
-var confirmLenght = (prompt("How many characters would you like your password to contain?"));
+var confirmLength = (prompt("How many characters would you like your password to contain?"));
+{
+    console.log(confirmLength);
+}
  
- if (confirmLength <= 7 || confirmLength >=129) {   alert("Password must be between 8-128 characters Try again");
- var confirmLenght = (prompt("How many characters would you like your password to contain?"));
+while (confirmLength < 8 || confirmLength > 128) {
+    alert("Password must be between 8-128 characters Try again");
+
+var confirmLength = (prompt("How many characters would you like your password to contain?"));   
 
 }
-    
 
 
-var confirmNumericCharacter = confirm("Click ok to confirm including numeric characters");
+
+ var confirmNumericCharacter = confirm("Click ok to confirm including numeric characters");
+
+
+
+
 var confirmLowercase = confirm("Click Ok to confirm including lowercase characters");
 var confirmUppercase = confirm("Click ok to confirm including uppercase characters");
 
